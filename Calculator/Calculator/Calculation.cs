7d594eba8aa2a032271602909e94824aba009377
Baburao Chavan <anjali.baburao-chavan@capgemini.com>
2022-09-15 23:29:18 +0530

    Multithreading Task-1 added

---
+++ Calculator/Calculator/Calculation.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculation
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        //System.Diagnostics.EventLog.CreateEventSource
+        public int num1,num2;
+        public Calculation(int a,int b)
+        {
+           num1 = a;
+           num2 = b;
+
+        }
+        public void add() 
+        {
+            int t = num1+num2;
+            Console.WriteLine("addition of two numbers is {0}", t);
+        }
+        public void subtract()
+        {
+            Thread.Sleep(2000);
+            int t = num1 - num2;
+            Console.WriteLine("subtraction of two numbers is {0}", t);
+        }
+        public void multiply()
+        {
+            Thread.Sleep(4000);
+            int t = num1 * num2;
+            Console.WriteLine("product of two numbers is {0}", t);
+        }
+        public void divide()
+        {
+            try
+            {
+                Thread.Sleep(6000);
+                int t = num1 / num2;
+                Console.WriteLine("division of two numbers is {0}", t);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error in division: {0}",ex.Message);
+                log.Error("Error in division "+ ex.Message);
+            }
+           
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter a:");
+            a = Convert.ToInt32(Console.ReadLine());   //take input from user
+            Console.WriteLine("Enter b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            Calculation c1 = new Calculation(a,b);     //creating instance of Calculation class.
+            Thread t1 = new Thread(c1.add);                
+            Thread t2 = new Thread(c1.subtract);       //invoking the instance methods for calculating.
+            Thread t3 = new Thread(c1.multiply);
+            Thread t4 = new Thread(c1.divide);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            Console.ReadLine();
+
+        }
+    }
+}
